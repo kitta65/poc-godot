60ec7ed6e728709efe1c708b8ce096dda22e2810
kitta65 <26474260+kitta65@users.noreply.github.com>
2025-06-08 17:18:43 +0900

    enable to load save data

--- main.gd
+++ main.gd
@@ -11,7 +11,7 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func _on_save_button_pressed() -> void:
-	var save_file = FileAccess.open(save_file_path, FileAccess.WRITE)
+	var file = FileAccess.open(save_file_path, FileAccess.WRITE)
 
 	var nodes := get_tree().get_nodes_in_group("save")
 	for node in nodes:
@@ -20,4 +20,32 @@ func _on_save_button_pressed() -> void:
 			continue
 		var data = node.to_save_data()
 		var json := JSON.stringify(data)
-		save_file.store_line(json)
+		file.store_line(json)
+
+
+func _on_load_button_pressed() -> void:
+	# rm all nodes
+	var nodes := get_tree().get_nodes_in_group("save")
+	for node in nodes:
+		node.queue_free()
+
+	# TODO: error handling (what if the file doesn't exist?)
+	var file = FileAccess.open(save_file_path, FileAccess.READ)
+	for line in file.get_as_text().split("\n"):
+		if line == "":
+			break
+
+		var json = JSON.new()
+		var parse_result = json.parse(line)
+		if not parse_result == OK:
+			printerr("Failed to parse JSON: %s" % line)
+			continue
+
+		match json.data["type"]:
+			"edge":
+				var edge = edge_scene.instantiate()
+				edge.position = Vector2(json.data["position.x"], json.data["position.y"])
+				add_child(edge)
+			_:
+				printerr("Unknown type in JSON: %s" % json.data["type"])
+				break
